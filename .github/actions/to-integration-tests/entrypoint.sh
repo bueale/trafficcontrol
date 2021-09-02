4fc779551ce9213c6e62fb3922f84c6f28c500d1
bueale <61953098+bueale@users.noreply.github.com>
2021-09-02 14:17:43 -0500

    updating IP to database

--- .github/actions/to-integration-tests/entrypoint.sh
+++ .github/actions/to-integration-tests/entrypoint.sh
@@ -51,9 +51,9 @@ start_traffic_vault() {
 		sed -i '/to-access\.sh\|^to-enroll/d' /etc/riak/{prestart.d,poststart.d}/*
 	BASH_LINES
 
-  #psql -d <user>://<dbname>:<password>:<port>/<?>
+  #psql -d <user>://<dbname>:<password>@<host>:<port>/<?> < <tables-doc> > /dev/null
   cd traffic_ops/app
-	psql -d traffic_vault://traffic_vault:twelve@localhost:5432/traffic_vault < db/trafficvault/create_tables.sql > /dev/null
+	psql -d traffic_vault://traffic_vault:twelve@172.17.0.1:5432/traffic_vault < db/trafficvault/create_tables.sql > /dev/null
 
 }
 truncate -s0 "${ciab_dir}/traffic.vault.logs";
